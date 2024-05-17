6a26969bb3996e7714d111b92ad254a989fe4aee
Eva Pace <eba.pachi@gmail.com>
2024-05-17 16:35:03 -0300

    base routing

--- src/capital_gain/controller.clj
+++ src/capital_gain/controller.clj
@@ -1,15 +1,35 @@
 (ns capital-gain.controller
   (:require ;;[capital-gain.business-logic :as business-logic]
             [capital-gain.adapters :as adapters]
-            ;;[capital-gain.database :as db]
-            ))
+            [capital-gain.database :as db]))
 
+(defn buy-stocks
+  "Buy stocks"
+  [storage trade]
+  ;; (db/buy-stocks! storage trade)
+  (print "buy")
+  trade)
 
-;; (defn execute-controller
-;;   "Executes controller with storage and user input."
-;;   [storage controller-and-input]
-;;   (let [[controller input-data] controller-and-input]
-;;       (controller storage input-data)))
+(defn sell-stocks
+  "Sell stocks"
+  [storage trade]
+  (print "sell")
+  trade)
+
+;; review
+(defn execute-controller
+  "Executes controller with storage and user input."
+  [storage controller-and-input]
+  (let [[controller input-data] controller-and-input]
+      (controller storage input-data)))
+
+(defn routing
+  "Routes to appropriate controller based of map key."
+  [trade]
+  (case (:operation trade)
+    "buy" [buy-stocks trade]
+    "sell" [sell-stocks trade]
+    [(constantly nil) trade]))
 
 (defn controller
   "Adapts and routes the user input to the correct controller, and executes it.
@@ -17,6 +37,6 @@
   [storage input]
   (->> input
        adapters/json->edn
-       ;; routing
-       ;; (execute-controller storage)
+       ;; ewww
+       (map (comp (partial execute-controller storage) routing))
        adapters/edn->json))
---
+++ src/capital_gain/database.clj
@@ -0,0 +1,3 @@
+(ns capital-gain.database
+  (:require [capital-gain.storage :as storage]))
+
